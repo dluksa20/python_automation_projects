fcdb129cc1b73daccd902da0d5e50a526da00819
Dovydas Luksa <dluksa20@gmail.com>
2024-08-25 23:30:54 +0100

    Add a message when everything is okay

---
+++ all_checks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+def check_reboot():
+    return os.path.exists('/run/reboot-required')
+
+def main():
+    if check_reboot():
+        print('Pending reboot')
+        sys.exit(1)
+    print('Everything is ok.')
+    sys.exit(0)
+main()
+
